de041704a8bb956152f4442bc202bdddb7cb6b9e
Smkboy123 <legenisam@gmail.com>
2024-07-15 09:22:45 +0000

    Message commit

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-accueil',
@@ -8,7 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './accueil.component.scss'
 })
 export class AccueilComponent {
-  constructor(){}
+  constructor(private router:Router){}
+  // gotoServices(){
+  //   this.router.navigate(['services'])
+  //   alert("Services")
+  // }
+  
+  currentAction:any;
+  setCurrentAction(action:any){
+    this.currentAction=action
+  }
 
   prendreTicket() {
     alert('Ticket pris avec succès!');
