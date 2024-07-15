37f5897da101923eb43a9984e9239b541b0d5c43
Aly-D-konte <alykonte19@gmail.com>
2024-07-15 17:39:21 +0200

    #Verification du commit

---
+++ src/app/accueil/accueil.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accueil',
+  standalone: true,
+  imports: [],
+  templateUrl: './accueil.component.html',
+  styleUrl: './accueil.component.scss'
+})
+export class AccueilComponent {
+  constructor(){}
+
+  prendreTicket() {
+    alert('Ticket pris avec succès!');
+  }
+}
